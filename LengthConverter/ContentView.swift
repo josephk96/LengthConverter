cb660c87afb6c3a7155e1437fdbd8e6b386f7ec4
Soo Hwan Kim <soohwan@shuttledelivery.co.kr>
2022-02-06 01:45:22 +0900

    Initial commit

--- LengthConverter/ContentView.swift
+++ LengthConverter/ContentView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputUnit = "meters"
+    @State private var outputUnit = "feet"
+    @State private var inputValue = 0
+    var outputValue = 0
+
+    let lengthOptions = ["meters", "km", "feet", "yards", "miles"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Choose input unit", selection: $inputUnit) {
+                        ForEach(lengthOptions, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+
+                Section {
+                    Picker("Choose output unit", selection: $outputUnit) {
+                        ForEach(lengthOptions, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section {
+                    TextField("Input Value", value: $inputValue, format: .number)
+                } header: {
+                    Text("Input Value")
+                }
+                
+                Section {
+                    Text(outputValue, format: .number)
+                } header: {
+                    Text("Output Value")
+                }
+            }
+        }
     }
 }
 
