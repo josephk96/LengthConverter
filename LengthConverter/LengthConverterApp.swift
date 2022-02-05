44947f132966d3c79c4ca67e3e59382808a185fd
Soo Hwan Kim <soohwan@shuttledelivery.co.kr>
2022-02-06 00:22:24 +0900

    Initial Commit

---
+++ LengthConverter/LengthConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConverterApp.swift
+//  LengthConverter
+//
+//  Created by Soo Hwan Kim on 2022/02/06.
+//
+
+import SwiftUI
+
+@main
+struct LengthConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
